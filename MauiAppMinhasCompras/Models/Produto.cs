eeebb835ccc4058b75d6cabb1b642932d0eeea67
sacastilho <sabrinadcastilho@gmail.com>
2025-09-07 17:12:26 -0300

    ajustes

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,48 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco {  get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            } 
+        }
+        public double Quantidade {
+            get => _quantidade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("A quantidade não pode ser negativa");
+                }
+
+                _quantidade = value;
+            }
+                }
+        public double Preco {
+            get => _preco;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("O preço não pode ser negativo");
+                }
+
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
